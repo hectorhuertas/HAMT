94e55933d09828dcd1dbce98675528837ee0af87
Hector Huertas <hectorhuertas@gmail.com>
2015-11-04 16:47:07 -0700

    core: set default key and value

--- lib/node.rb
+++ lib/node.rb
@@ -1,2 +1,8 @@
 class Node
+  attr_reader :key, :value
+
+  # def initialize
+  #   @key = nil
+  #   @value = nil
+  # end
 end
--- test/node_test.rb
+++ test/node_test.rb
@@ -2,7 +2,23 @@ require 'node'
 
 class NodeTest < Minitest::Test
 
+  def setup
+    @node = Node.new
+  end
+
   def test_it_exists
     assert Node
   end
+
+  def test_it_is_created_without_a_key
+    refute @node.key
+  end
+
+  def test_it_is_created_without_a_value
+    refute  @node.value
+  end
+
+
+  # def test_it_is_created_with_its_e
+
 end
