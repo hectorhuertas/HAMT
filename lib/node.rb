3a789ac7aa93b2bd3e870a8ea3f26625afc785f0
Hector Huertas <hectorhuertas@gmail.com>
2015-11-05 16:42:15 -0700

    refac: hide Index use

--- lib/node.rb
+++ lib/node.rb
@@ -8,21 +8,25 @@ class Node
     @depth = depth
   end
 
-  def set(key_input, value_input)
-    if key == key_input || key.nil?
+  def set(input, value_input)
+    if key == input || key.nil?
       @value = value_input
-      @key   = key_input
+      @key   = input
     else
-      @links[Index.get(key_input, depth)] ||= Node.new(depth + 1)
-      @links[Index.get(key_input, depth)].set(key_input, value_input)
+      @links[index_of(input)] ||= Node.new(depth + 1)
+      @links[index_of(input)].set(input, value_input)
     end
   end
 
-  def get(key_input)
-    if key_input == key
+  def get(input)
+    if input == key
       @value
     else
-      @links[Index.get(key_input, depth)].get(key_input)
+      @links[index_of(input)].get(input)
     end
+  end
+
+  def index_of(input)
+    Index.get(input, depth)
   end
 end
