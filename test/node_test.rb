2016cd6b65c20dbe4cbaa250c3640bb3eaae0075
Hector Huertas <hectorhuertas@gmail.com>
2015-11-04 22:47:17 -0700

    core: gets the value of a second node

--- test/node_test.rb
+++ test/node_test.rb
@@ -19,71 +19,77 @@ class NodeTest < Minitest::Test
   end
 
   def test_it_sets_a_key_value
-    @node.set('Pizza', 'Yummy')
+    @node.set('pizza', 'Yummy')
 
     assert_equal 'Yummy', @node.value
   end
 
   def test_it_sets_other_key_value
-    @node.set('Brocoli', 'Ahgg')
+    @node.set('brocoli', 'Ahgg')
 
     assert_equal 'Ahgg', @node.value
   end
 
   def test_it_gets_a_value_from_its_key
-    @node.set('Pizza', 'Yummy')
+    @node.set('pizza', 'Yummy')
 
-    assert_equal 'Yummy', @node.get('Pizza')
+    assert_equal 'Yummy', @node.get('pizza')
   end
 
   def test_it_gets_another_value_from_its_key
-    @node.set('Brocoli', 'Ahgg')
+    @node.set('brocoli', 'Ahgg')
 
-    assert_equal 'Ahgg', @node.get('Brocoli')
+    assert_equal 'Ahgg', @node.get('brocoli')
   end
 
   def test_it_overrides_the_value_of_a_key
-    @node.set('Pizza', 'Yummy')
-    @node.set('Pizza', 'Ahgg')
+    @node.set('pizza', 'Yummy')
+    @node.set('pizza', 'Ahgg')
 
-    assert_equal 'Ahgg', @node.get('Pizza')
+    assert_equal 'Ahgg', @node.get('pizza')
 
   end
 
   def test_it_overrides_the_value_of_another_key
-    @node.set('Pizza', 'Ahgg')
-    @node.set('Pizza', 'Yummy')
+    @node.set('pizza', 'Ahgg')
+    @node.set('pizza', 'Yummy')
 
-    assert_equal 'Yummy', @node.get('Pizza')
+    assert_equal 'Yummy', @node.get('pizza')
   end
 
   def test_it_doesnt_overide_a_value_of_a_different_key
-    @node.set('Pizza', 'Yummy')
-    @node.set('Brocoli', 'Ahgg')
+    @node.set('pizza', 'Yummy')
+    @node.set('brocoli', 'Ahgg')
 
-    assert_equal 'Yummy', @node.get('Pizza')
+    assert_equal 'Yummy', @node.get('pizza')
   end
 
   def test_it_sets_a_second_key_value_on_its_position
-    @node.set('Pizza', 'Yummy')
+    @node.set('pizza', 'Yummy')
     @node.set('brocoli', 'Ahgg')
 
     assert_equal 'Ahgg', @node.links[25].value
   end
 
   def test_it_sets_another_second_key_value_on_its_position
-    @node.set('Pizza', 'Yummy')
+    @node.set('pizza', 'Yummy')
     @node.set('calzone', 'Real Yummy!')
 
     assert_equal 'Real Yummy!', @node.links[15].value
   end
 
   def test_it_gets_the_value_of_a_second_key
-    skip
+    @node.set('pizza', 'Yummy')
+    @node.set('calzone', 'Real Yummy!')
+
+    assert_equal 'Real Yummy!', @node.links[15].value
   end
 
   def test_it_gets_the_value_of_another_second_key
-    skip
+    @node.set('calzone', 'Real Yummy!')
+    @node.set('brocoli', 'Ahgg')
+
+    assert_equal 'Ahgg', @node.links[25].value
   end
 
   def test_it_SHA1_hashes_a_value
