721d81cbf236677daec6a511fe7b86a81264cb23
Hector Huertas <hectorhuertas@gmail.com>
2015-11-05 17:10:34 -0700

    refac: subdivide node methods

--- lib/node.rb
+++ lib/node.rb
@@ -1,5 +1,3 @@
-# require 'digest'
-
 class Node
   attr_reader :key, :value, :links, :depth
 
@@ -9,12 +7,10 @@ class Node
   end
 
   def set(input, value_input)
-    if key == input || key.nil?
-      @value = value_input
-      @key   = input
-    else
-      @links[index_of(input)] ||= Node.new(depth + 1)
-      @links[index_of(input)].set(input, value_input)
+    case key
+    when nil   then asign_to_this_node(input, value_input)
+    when input then asign_to_this_node(input, value_input)
+    else            asign_to_other_node(input, value_input)
     end
   end
 
@@ -22,11 +18,34 @@ class Node
     if input == key
       @value
     else
-      @links[index_of(input)].get(input)
+      link_of(input).get(input)
     end
+  end
+
+  def asign_to_this_node(input, value_input)
+    @value = value_input
+    @key   = input
+  end
+
+  def asign_to_other_node(input, value_input)
+    initialize_link(input)
+    asign_to_linked_node(input, value_input)
+  end
+
+  def initialize_link(input)
+    @links[index_of(input)] ||= Node.new(depth + 1)
+  end
+
+  def asign_to_linked_node(input, value_input)
+    link_of(input).set(input, value_input)
+  end
+
+  def link_of(input)
+    @links[index_of(input)]
   end
 
   def index_of(input)
     Index.get(input, depth)
   end
+
 end
